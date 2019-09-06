251d7e7e8a4c902d03081ac15bdd91b0096a44de
gt22 <igorengel@mail.ru>
2019-09-06 15:50:00 +0300

    ci

---
+++ texlive/build_all.py
@@ -0,0 +1,20 @@
+import os
+import subprocess as sp
+
+def build(d):
+    for a in os.listdir(d):
+        pt = os.path.join(d, a)
+        if a.endswith('.tex'):
+            print(f'Compiling {pt}...')
+            out_dir = os.path.join('_build', d[d.index('/') + 1:]) #cut src/ from output dir
+            os.makedirs(os.path.join(out_dir), exist_ok=True)
+            sp.call(['pdflatex', '-output-directory', out_dir, pt], stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+            for out in os.listdir(out_dir):
+                if not out.endswith('.pdf'):
+                    os.remove(os.path.join(out_dir, out))
+        else:
+            if os.path.isdir(pt):
+                build(pt)
+
+
+build('src/')
