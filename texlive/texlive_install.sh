040744c88709de3cd39b00de903ae795803a868c
gt22 <igorengel@mail.ru>
2019-09-06 12:16:33 +0300

    ci

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -20,7 +20,7 @@ if ! command -v texlua > /dev/null; then
 
   cd ..
 fi
-cp `find ./packages/preamble -f -name "*.sty"` .
+cp `find ./packages/preamble -f -name "*.sty"` ./
 ls -l
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
