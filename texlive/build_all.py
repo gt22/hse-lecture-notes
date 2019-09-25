630c80bd6074b68e2343132b0e6cf301f6d3b149
gt22 <igorengel@mail.ru>
2019-09-25 20:38:06 +0300

    update build script

--- texlive/build_all.py
+++ texlive/build_all.py
@@ -8,7 +8,7 @@ def build(d):
             print(f'Compiling {pt}...')
             out_dir = os.path.abspath(os.path.join('_build', d[d.index('/') + 1:])) #cut src/ from output dir
             os.makedirs(os.path.join(out_dir), exist_ok=True)
-            sp.call(['pdflatex', '-output-directory', out_dir, a], cwd=d, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+            sp.call(['pdflatex', '-output-directory', out_dir, a], cwd=d)
             for out in os.listdir(out_dir):
                 if not out.endswith('.pdf'):
                     os.remove(os.path.join(out_dir, out))
