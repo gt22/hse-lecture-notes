85bd356f9b6f3e1391ee0f659f19dc5a93a7fa83
gt22 <igorengel@mail.ru>
2019-09-25 21:59:45 +0300

    update build script

--- texlive/build_all.py
+++ texlive/build_all.py
@@ -1,17 +1,29 @@
 import os
+import shutil
 import subprocess as sp
 
+def copy(f, d):
+    ret = []
+    for file_name in os.listdir(f):
+        full_file_name = os.path.join(f, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, d)
+            ret.append(file_name)
+    return ret
 def build(d):
     for a in os.listdir(d):
         pt = os.path.join(d, a)
         if a.endswith('.tex'):
             print(f'Compiling {pt}...')
+            extra_files = copy(d, '.')
             out_dir = os.path.abspath(os.path.join('_build', d[d.index('/') + 1:])) #cut src/ from output dir
             os.makedirs(os.path.join(out_dir), exist_ok=True)
-            sp.call(['pdflatex', '-output-directory', out_dir, a], cwd=d)
+            sp.call(['pdflatex', '-output-directory', out_dir, a], stdout=sp.DEVNULL, stderr=sp.DEVNULL)
             for out in os.listdir(out_dir):
                 if not out.endswith('.pdf'):
                     os.remove(os.path.join(out_dir, out))
+            for ff in extra_files:
+                os.remove(ff)
         else:
             if os.path.isdir(pt):
                 build(pt)
