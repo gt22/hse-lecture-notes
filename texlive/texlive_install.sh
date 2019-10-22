b56a4da01d7839cbc94f22218f212c99521b6342
gt22 <igorengel@mail.ru>
2019-10-22 11:04:45 +0300

    update scripts

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -27,7 +27,7 @@ tlmgr install luatex
 
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
-tlmgr install amsfonts listings xcolor pgfplots amsmath oberdiek babel-russian cyrillic collection-langcyrillic accents
+tlmgr install amsfonts listings xcolor pgfplots amsmath oberdiek babel-russian cyrillic collection-langcyrillic accent tikzs
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
