fbce67383bba2d3cfa788604f3023c39399926eb
gt22 <igorengel@mail.ru>
2020-06-08 19:15:38 +0300

    fix logic questions format

--- src/term2-logic-questions/switch.sh
+++ src/term2-logic-questions/switch.sh
@@ -1,10 +1,1 @@
-if [ -e conspect.tex.latexmain ]
-then
-    rm conspect.tex.latexmain
-    touch conspect_lectures.tex.latexmain
-    echo "Switching to lectures"
-else
-    rm conspect_lectures.tex.latexmain
-    touch conspect.tex.latexmain
-    echo "Switching to normal"
-fi
+../term0-template/switch.sh
